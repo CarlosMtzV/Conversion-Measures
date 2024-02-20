4fc0c90388245337599d8bd533214e2a76ff6a82
CarlosMtzV <159225079+CarlosMtzV@users.noreply.github.com>
2024-02-20 01:53:53 -0600

    Conversion Measures

---
+++ MedidasConvercion/Conversiones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidasConvercion
+{
+    internal class Conversiones
+    {
+        // Método
+        public static double MetrosACentimetros(double metros)
+        {
+            return metros * 100;
+        }
+
+        // Método 
+        public static double MetrosAPulgadas(double metros)
+        {
+            return metros * 39.3701;
+        }
+
+        // Método 
+        public static double MetrosAPies(double metros)
+        {
+            return metros * 3.28084;
+        }
+    }
+}
---
+++ MedidasConvercion/Program.cs
@@ -0,0 +1,13 @@
+﻿using MedidasConvercion;
+
+Console.Write("Enter the number of meters: ");
+double metros = Convert.ToDouble(Console.ReadLine());
+
+double centimetros = Conversiones.MetrosACentimetros(metros);
+double pulgadas = Conversiones.MetrosAPulgadas(metros);
+double pies = Conversiones.MetrosAPies(metros);
+
+Console.WriteLine(metros + " meters are equivalent to:");
+Console.WriteLine( centimetros + " cm");
+Console.WriteLine(pulgadas + " pulg");
+Console.WriteLine(pies +" ft");
